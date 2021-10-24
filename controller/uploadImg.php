9c0c1df4396be76251faa5b77afb8a8fa76b0f0f
xuanco941 <68849866+xuanco941@users.noreply.github.com>
2021-10-24 10:21:03 +0700

    update upload img hotel

--- controller/uploadImg.php
+++ controller/uploadImg.php
@@ -1,14 +1,14 @@
 <?php
+function upImg($img){
 $target_dir = "../uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($img["name"]);
 $uploadOk = 1; // neu = 0 thi khong cho up
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Kiem tra xem co phai file anh khong
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  $check = getimagesize($img["tmp_name"]);
   if($check !== false) {
-    echo "Day la anh- " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
     echo "Day khong phai anh";
@@ -17,13 +17,13 @@ if(isset($_POST["submit"])) {
 }
 
 // neu filr ton tai roi khi set $uploadOk = 0
-if (file_exists($target_file)) {
-  echo "File da ton tai.";
-  $uploadOk = 0;
-}
+// if (file_exists($target_file)) {
+//   echo "File da ton tai.";
+//   $uploadOk = 0;
+// }
 
 // gioi han kich thuoc anh
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($img["size"] > 500000) {
   echo "kich thuoc qua lon";
   $uploadOk = 0;
 }
@@ -31,18 +31,16 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 // chi cho upfile jpg , png , jpeg , gif
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "chi cho upfile jpg , png , jpeg , gif";
+  echo 'chi cho upfile jpg , png , jpeg , gif';
   $uploadOk = 0;
 }
 
 // Neu uploadOk = 1 thi cho up
 if ($uploadOk == 0) {
-  echo "Loi up";
+  return false;
 } else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Loi up";
-  }
+  move_uploaded_file($img["tmp_name"], $target_file);
+  return true;
+}
 }
 ?>
