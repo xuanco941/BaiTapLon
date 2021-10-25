4361929aec39a3b709185d96dc94a04c12eb614f
xuanco941 <68849866+xuanco941@users.noreply.github.com>
2021-10-25 21:57:26 +0700

    reponsive mb

--- views/error.php
+++ views/error.php
@@ -7,21 +7,19 @@
     <link rel="stylesheet" href="/assets/css/styleUserHome.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="../assets/css/header.css">
     <title>Document</title>
 </head>
 <body>
     
-    <?php include './partials/header.php' ?>
-
     <div class="text-center" style="height: 42rem;">
         <div class="container">
             <p><i class="fal fa-bug" style="font-size: 20rem; margin-top:8%; color: #adb5bd;"></i></p>
             <br>
-            <p style="color: #adb5bd;">OPP!!! We've noted an error while using your app. <br> Please return to the homepage to continue the selection.</p>
-            <a class="btn btn-warning" href="http://localhost/BTL_CNW_QLKS/" role="button"><button style="color: inherit; background-color:inherit; border:none;" type="submit"><i class="fal fa-home-lg"></i> Home Page</button></a>
+            <p style="color: #adb5bd;">Rất tiếc, <br> Hệ thống đang có lỗi gì đó...</p>
+            <a class="btn btn-warning" href="../index.php" role="button"><button style="color: inherit; background-color:inherit; border:none;" type="submit"><i class="fal fa-home-lg"></i> Home Page</button></a>
         </div>
     </div>
-    <?php include './partials/footer.php' ?>
 </body>
 </html>
 
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,39 +1,67 @@
-<nav class="navbar navbar-expand-md navbar-light sticky-top " style="background-color: #e3f2fd;">
+
+
+<nav id="navbar-pc" class="navbar navbar-expand-md navbar-light sticky-top " style="background-color: #e3f2fd;">
     <div class="container-fluid">
-        <a class="navbar-branch" href="#">
-            <img src="image/mau-logo-khach-san-dep-1.png" height="50">
-        </a>
+        
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="./home.php">Home</a>
+                <li class="nav-item item-head">
+                    <a class="nav-link active" href="index.php">Trang chủ</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact us</a>
+                <li class="nav-item item-head">
+                    <a class="nav-link" href="./ticket.php">Đặt vé nhanh</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Make a reservation</a>
-                </li>
-
             </ul>
 
             <ul class="navbar-nav ms-auto">
                 <li>
                     <form action="../controller/searchHotel.php" method="GET" class="input-group">
-                        <input type="text" class="form-control" placeholder="search" aria-label="search" name="hotel_name">
-                        <a class="btn btn-warning" href="#" role="button"><button style="color: inherit; background-color:inherit; border:none;" type="submit">search</button></a>
+                        <input type="text" class="form-control" placeholder="Nhập tên khách sạn" aria-label="search" name="hotel_name">
+                        <a class="btn btn-primary" href="#" role="button"><button style="color: inherit; background-color:inherit; border:none;" type="submit">Tìm kiếm</button></a>
                     </form>
                 </li>
             </ul>
 
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="btn btn-secondary" href="#" role="button"><i class="fas fa-cart-plus"></i>cart</a>
-                    <a class="btn btn-warning" href="./signin.php" role="button">Sign in</a>
-                    <a class="btn btn-warning" href="./signup.php" role="button">Sign up</a>
+                    <a href="./ticket.php" class="btn btn-info" style="color: blue;" href="#" role="button"><i class="fas fa-cart-plus"></i>Vé đặt</a>
+                    <?php
+                    if (!isset($_SESSION['loginSuccess'])) {
+                        echo '
+                <a style="color: white;" class="btn btn-success" href="./signin.php" role="button">Đăng nhập</a>
+                <a style="color: white;" class="btn btn-primary" href="./signup.php" role="button">Đăng ký</a>
+                ';
+                    } else {
+                        echo '<a style="color: white;" class="btn btn-danger" href="../controller/signout.php" role="button">Đăng xuất</a>';
+                    }
+                    ?>
                 </li>
 
             </ul>
         </div>
+    </div>
+</nav>
+
+<nav id="navbar-mb" class="navbar navbar-expand-md navbar-light sticky-top " style="background-color: #e3f2fd;">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <li><button class="dropdown-item" type="button">Đặt vé nhanh</button></li>
+            <li><a class="dropdown-link" href="./ticket.php"><button class="dropdown-item" type="button">Vé đặt</button></a></li>
+            <li><a class="dropdown-link" href="./search.php"><button class="dropdown-item" type="button">Tìm kiếm</button></a></li>
+            <?php
+            if (!isset($_SESSION['loginSuccess'])) {
+                echo '
+          <li  ><a class="dropdown-link" href="./signin.php" ><button class="dropdown-item" type="button">Đăng nhập</button></a></li>
+          <li  ><a class="dropdown-link" href="./signup.php"><button class="dropdown-item" type="button">Đăng ký</button></a></li>
+                ';
+            } else {
+                echo '<li  ><a class="dropdown-link" href=../controller/signout.php><button class="dropdown-item" type="button">Đăng xuất</button></a></li>';
+            }
+            ?>
+
+        </ul>
     </div>
 </nav>
