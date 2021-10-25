623cbd202f898fd9d58923c1270e6ac6eb5fdb7a
tranvantuann021201 <tvtuan0212@gmail.com>
2021-10-25 17:12:26 +0700

    error page

---
+++ views/error.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/styleUserHome.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php include './partials/header.php' ?>
+
+    <div class="text-center" style="height: 42rem;">
+        <div class="container">
+            <p><i class="fal fa-bug" style="font-size: 20rem; margin-top:5%; color: #adb5bd;"></i></p>
+            <br>
+            <p style="color: #adb5bd;">OPP!!! We've noted an error while using your app. <br> Please return to the homepage to continue the selection.</p>
+            <a class="btn btn-warning" href="http://localhost/BTL_CNW_QLKS/" role="button"><button style="color: inherit; background-color:inherit; border:none;" type="submit"><i class="fal fa-home-lg"></i> Home Page</button></a>
+        </div>
+    </div>
+    <?php include './partials/footer.php' ?>
+</body>
+</html>
+
